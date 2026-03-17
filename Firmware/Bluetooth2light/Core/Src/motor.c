4e2bcbb0543a66de4bde617a691e5da8b0a932f0
fredellapang <127003024+fredellapang@users.noreply.github.com>
2026-03-17 16:05:51 -0600

    idk what the changes to bluetooth were

---
+++ Firmware/Bluetooth2light/Core/Src/motor.c
@@ -0,0 +1,120 @@
+/*
+ * motor.c
+ *
+ *  Created on: Mar 17, 2026
+ *      Author: frede
+ */
+
+#include <motor.h>
+
+extern TIM_HandleTypeDef MOTOR_TIMER;
+
+void Motor_Init(){
+	TIM1->CCR1=300;
+	HAL_TIM_PWM_Start (&MOTOR_TIMER, MOTOR1_FCHANNEL);
+	HAL_TIM_PWM_Start (&MOTOR_TIMER, MOTOR1_BCHANNEL);
+}
+
+int time = 1000;
+
+void Set_Motor_Speed(int16_t speed ,  Motor_Driver driver, Direction dir) {
+    // Speed range: -100 to 100
+    uint32_t pulse_value = (abs(speed) * __HAL_TIM_GET_AUTORELOAD(&MOTOR_TIMER)) / 100;
+    MOTOR_TIMER_INSTANCE->CCR1=300;
+
+
+		__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value);
+		__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value);
+			if (speed > 0) {
+				// Forward: Pulse IN1, keep IN2 Low
+//				__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value);
+//				__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, 0);
+				if (dir == STRAIGHT){
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value);
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value);
+				}else if (dir == RIGHT){
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value/30);
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value);
+				}else{
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value);
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value/30);
+				}
+				HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_F_PIN, 1);
+				HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_B_PIN, 0);
+				HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_F_PIN, 1);
+				HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_B_PIN, 0);
+				HAL_Delay(time);
+
+			}
+			else if (speed < 0) {
+				// Reverse: Keep IN1 Low, pulse IN2
+				if (dir == STRAIGHT){
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value);
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value);
+				}else if (dir == LEFT){
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value/2);
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value);
+				}else{
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value);
+					__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value/2);
+				}
+				HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_F_PIN, 0);
+				HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_B_PIN, 1);
+				HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_F_PIN, 0);
+				HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_B_PIN, 1);
+				HAL_Delay(time);
+
+			}
+			else {
+				// Stop: Both
+				__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, 400);
+				__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, 400);
+				if (dir == STRAIGHT){
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_F_PIN, 1);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_B_PIN, 1);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_F_PIN, 1);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_B_PIN, 1);
+				}else if (dir == LEFT){
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_F_PIN, 0);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_B_PIN, 1);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_F_PIN, 1);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_B_PIN, 0);
+				}else{
+
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_F_PIN, 1);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR1_B_PIN, 0);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_F_PIN, 0);
+					HAL_GPIO_WritePin(MOTOR_PORT, MOTOR2_B_PIN, 1);
+				}
+				HAL_Delay(time);
+			}
+			// ADD STOP BOTH LOW??(Low-Power Sleep Mode after 1ms)
+		}
+
+
+
+//	if (driver == PCB){ // pcb driver
+////		if (speed > 0) {
+////			// Forward: Pulse IN1, keep IN2 Low
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, 0);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR2_FCHANNEL, pulse_value);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR2_BCHANNEL, 0);
+////		}
+////		else if (speed < 0) {
+////			// Reverse: Keep IN1 Low, pulse IN2
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, 0);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR2_FCHANNEL, 0);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR2_BCHANNEL, pulse_value);
+////		}
+////		else {
+////			// Stop: Both
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_FCHANNEL, pulse_value);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR1_BCHANNEL, pulse_value);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR2_FCHANNEL, pulse_value);
+////			__HAL_TIM_SET_COMPARE(&MOTOR_TIMER, MOTOR2_BCHANNEL, pulse_value);
+////		}
+//		// Low (Low-Power Sleep Mode after 1ms)
+//	}
+//	else{ // red driver
